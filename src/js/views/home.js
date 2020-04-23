a1860885ddba6827a0574697c1e72cd154e76d07
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-04-23 23:14:33 +0000

    update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,37 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.scss";
+import navbar from "./navbar";
+import cards from "./cards";
+import data from "./data";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+let va [
+   
+    "https://i.imgur.com/r7BkgqL.png",
+    "https://i.imgur.com/XoRnSGT.png",
+    "https://i.imgur.com/XoRnSGT.png",
+    "https://i.imgur.com/eJznJUg.png",
+        "https://i.imgur.com/hPdaRoZ.png",
+        "https://i.imgur.com/4jJSUbG.png",
+        "https://i.imgur.com/4jJSUbG.png",
+        "https://i.imgur.com/KggQ7wb.png"
+
+
+
+];
+
+
+
+export function Home() {
+	return (
+		<>
+			<Navbar />
+			<Jumbotron />
+
+			<div className="cards-group d-flex justify-content-center">
+				{va.map((item, index) => (
+					<Cards key={index} myimage={item} />
+				))}
+			</div>
+		</>
+	);
+}
+e
